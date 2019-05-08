b8e97ff2c0a9a0f518fbb8e40f63d7b830e41b24
jimmy818 <jmmkaler@gmail.com>
2019-05-08 15:01:04 +0530

    Google RedirectIs

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import ReactGA from 'react-ga'
 import mixpanel from 'mixpanel-browser'
 import MixpanelProvider from 'react-mixpanel'
 import { PersistGate } from 'redux-persist/integration/react'
-
+import HttpsRedirect from 'react-https-redirect';
 mixpanel.init("70d40c11476f564b54bc2cdca8c51526")
 
 ReactGA.initialize('UA-108603873-1', {
@@ -28,9 +28,11 @@ const { store, persistor } = configureStore()
 ReactDOM.render(
     <MixpanelProvider mixpanel={mixpanel}>
         <Provider store={store}>
-        	<PersistGate loading={null} persistor={persistor}>
-            	<Router history={browserHistory} routes={routes} onUpdate={logPageView}/>
-            </PersistGate>
+            <HttpsRedirect>
+                <PersistGate loading={null} persistor={persistor}>
+                    <Router history={browserHistory} routes={routes} onUpdate={logPageView}/>
+                </PersistGate>
+            </HttpsRedirect>
         </Provider>
     </MixpanelProvider>
     , document.getElementById('root'))
